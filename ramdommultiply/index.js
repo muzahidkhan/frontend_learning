62bf02f9f9725900662a9d23542424fc68be04d9
Md Muzahid Khan <muzahidkhan2k3@yahoo.com>
2024-07-15 14:47:34 +0530

    initial commit

---
+++ ramdommultiply/index.js
@@ -0,0 +1,40 @@
+const num1=Math.ceil(Math.random()*10)
+const num2=Math.ceil(Math.random()*10)
+
+
+const QuestionEL=document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl=document.getElementById("input");
+const scoreEl=document.getElementById("score");
+
+let score=JSON.parse(localStorage.getItem("score"));
+
+if(!score){
+    score=0;
+}
+
+scoreEl.innerText= "Score : "+score;
+QuestionEL.innerHTML = 'What is '+num1+ ' multiply by '+num2; 
+
+const correctAns = num1 * num2;
+
+
+formEl.addEventListener("submit",()=>{
+    const userAns= +inputEl.value
+    if(userAns===correctAns){
+    score++;
+    console.log(score);
+    updatelocalStorage()
+    }
+    else {
+    score--;
+    console.log(score);
+    updatelocalStorage()
+    }
+    }
+)
+
+function updatelocalStorage()
+{
+    localStorage.setItem("score",JSON.stringify(score));
+}
